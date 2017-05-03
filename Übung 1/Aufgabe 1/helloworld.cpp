9754c0a015afd52fb0ca3aba0692972b9ea68250
Max Weffers <rcstar6696@gmail.com>
2017-05-03 12:11:29 +0200

    Hello World Aufgabe 1

---
+++ Übung 1/Aufgabe 1/helloworld.cpp
@@ -0,0 +1,13 @@
+// helloworld.cpp
+
+#include <iostream>
+using namespace std;
+
+
+ // Dies ist mein erstes C++-Programm
+int main()
+ {
+ cout << " Hello World! " << endl;
+ system("PAUSE");
+ return 0;
+ }

dd564d16213a167bc2cedb53c6827129f006e15f
Max Weffers <rcstar6696@gmail.com>
2017-05-03 13:07:51 +0200

    Error.cpp vom Blatt

---
+++ Übung 1/Aufgabe 3/Errors.cpp
@@ -0,0 +1,27 @@
+// Program converts inch to cm
+#include <iostreaam>
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main()
+{
+	const float INTOCM = 2.54 // cm per inch
+			float numIn,
+			numCm;
+
+	// Request speed from user
+	cout << "Enter value in inch "
+		<< "to convert to cm: ";
+
+	cin << numIn;
+
+	// convert inch to cm
+	numIn = numIn * INTOCM;
+
+	// Output converted value
+	cout << "The speed in cm is " << numCm;
+	cout << endl;
+	system("PAUSE");
+	return 0;
+}
